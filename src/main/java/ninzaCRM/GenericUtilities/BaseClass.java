9884ddd27e2e36c49e960cddfe24547286d2d221
shama788 <shamabanum051@gmail.com>
2025-06-19 16:29:05 +0530

    CreateLead Test Changes committed

--- src/main/java/ninzaCRM/GenericUtilities/BaseClass.java
+++ src/main/java/ninzaCRM/GenericUtilities/BaseClass.java
@@ -15,6 +15,9 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 
+import ninzaCRM.ObjectRepository.CampaignPage;
+import ninzaCRM.ObjectRepository.LoginPage;
+
 
 
 /**
@@ -23,7 +26,7 @@ import org.testng.annotations.Parameters;
  *
  */
 public class BaseClass {
-	
+
 	public ExcelFileUtility eUtil = new ExcelFileUtility();
 	public JavaUtility jUtil = new JavaUtility();
 	public PropertyFileUtility pUtil = new PropertyFileUtility();
@@ -34,7 +37,7 @@ public class BaseClass {
 	//For listeners
 	public static WebDriver sdriver;
 	
-	@BeforeSuite(groups = {"SmokeSuite","RegressionSuite"})
+	@BeforeSuite(alwaysRun = true)
 	public void bsConfig()
 	{
 		System.out.println("======== DB Connection Successfull =========");
@@ -75,8 +78,8 @@ public class BaseClass {
 		String USERNAME = pUtil.readDataFromPropertyFile("username");
 		String PASSWORD = pUtil.readDataFromPropertyFile("password");
 		
-	//	LoginPage lp = new LoginPage(driver);
-	//	lp.loginToApp(USERNAME, PASSWORD);
+		LoginPage lp = new LoginPage(driver);
+		lp.loginToApp(USERNAME, PASSWORD);
 		
 		System.out.println("======== Login to App Successfull =========");
 	}
@@ -84,8 +87,9 @@ public class BaseClass {
 	@AfterMethod(alwaysRun = true)
 	public void amConfig() throws InterruptedException
 	{
-	//	HomePage hp = new HomePage(driver);
-	//	hp.logoutOfApp(driver);
+	
+		CampaignPage cp = new CampaignPage(driver);
+		cp.logoutOfApp(driver);
 		
 		System.out.println("======== Logout of App Successfull =========");
 	}
